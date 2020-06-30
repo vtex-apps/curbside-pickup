faa3176cca15ba4f0e16b8e53ff4e5b70e5bafec
Wender Lima <wender.lima@vtex.com.br>
2020-06-30 18:08:15 -0300

    Adjusting admin page to link to the app settings dynamically

--- react/admin/index.tsx
+++ react/admin/index.tsx
@@ -4,17 +4,18 @@ import {
   PageHeader,
 } from 'vtex.styleguide'
 
-
+const getAppId = () => {
+  return process.env.VTEX_APP_ID
+}
 
 const CurbsideIndex: FC = props => {
-
   return (
     <Layout
       pageHeader={
         <div className="flex justify-center">
           <div className="w-100 mw-reviews-header">
             <PageHeader
-              title="Curbside/Pickup"
+              title="Curbside Pickup"
              />
           </div>
         </div>
@@ -24,7 +25,13 @@ const CurbsideIndex: FC = props => {
         <div className="flex justify-center">
           <div className="w-100 mw-reviews-content pb8">
             <div className="mb7">
-              Test
+              <ul>
+                <li><a href="#">Thanks email template</a></li>
+                <li><a href="#">Handover email template</a></li>
+                <li><a href="#">Notified email template</a></li>
+                <li><a href="#">Cancelled email template</a></li>
+                <li><a href={`/admin/apps/${getAppId()}/setup`}>App settings</a></li>
+              </ul>
             </div>
             {props.children}
           </div>
