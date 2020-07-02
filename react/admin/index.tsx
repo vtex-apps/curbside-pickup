99bb95250d5acb33ab493b458d4de9bc03573658
Wender Lima <wender.lima@vtex.com.br>
2020-07-02 15:49:49 -0300

    Adding translation to the admin labels

--- react/admin/index.tsx
+++ react/admin/index.tsx
@@ -1,43 +1,70 @@
 import React, { FC } from 'react'
-import {
-  Layout,
-  PageHeader,
-} from 'vtex.styleguide'
+import { Layout, PageHeader, Box } from 'vtex.styleguide'
+import { injectIntl, FormattedMessage } from 'react-intl'
 
 const getAppId = () => {
-  return process.env.VTEX_APP_ID
+  return process.env.VTEX_APP_ID || ''
 }
 
-const CurbsideIndex: FC = props => {
+const CurbsideIndex: FC = ({ intl }: any) => {
   return (
     <Layout
       pageHeader={
         <div className="flex justify-center">
           <div className="w-100 mw-reviews-header">
             <PageHeader
-              title="Curbside Pickup"
-             />
+              title={intl.formatMessage({ id: 'admin/curbside.title' })}
+            />
           </div>
         </div>
       }
       fullWidth
     >
-        <div className="flex justify-center">
-          <div className="w-100 mw-reviews-content pb8">
-            <div className="mb7">
-              <ul>
-                <li><a href="/admin/message-center/#/templates/curbside-at-location">"At location" email template</a></li>
-                <li><a href="/admin/message-center/#/templates/curbside-cancel-order">"Cancel order" email template</a></li>
-                <li><a href="/admin/message-center/#/templates/curbside-package-ready">"Package ready" email template</a></li>
-                <li><a href="/admin/message-center/#/templates/curbside-ready-for-packing">"Ready for packing" email template</a></li>
-                <li><a href={`/admin/apps/${getAppId()}/setup`}>App settings</a></li>
-              </ul>
-            </div>
-            {props.children}
-          </div>
-        </div>
+      <div className="bg-muted-5 pa4 w-50">
+        <Box
+          title={intl.formatMessage({ id: 'admin/curbside.templates.title' })}
+          fit="none"
+        >
+          <ul className="mid-gray">
+            <li className="mb4">
+              <a href="/admin/message-center/#/templates/curbside-at-location">
+                <FormattedMessage id="admin/curbside.template.at-location" />
+              </a>
+            </li>
+            <li className="mb4">
+              <a href="/admin/message-center/#/templates/curbside-cancel-order">
+                <FormattedMessage id="admin/curbside.template.cancel-order" />
+              </a>
+            </li>
+            <li className="mb4">
+              <a href="/admin/message-center/#/templates/curbside-package-ready">
+                <FormattedMessage id="admin/curbside.template.package-ready" />
+              </a>
+            </li>
+            <li className="mb4">
+              <a href="/admin/message-center/#/templates/curbside-ready-for-packing">
+                <FormattedMessage id="admin/curbside.template.ready-for-packing" />
+              </a>
+            </li>
+          </ul>
+        </Box>
+      </div>
+      <div className="bg-muted-5 pa4 w-50">
+        <Box
+          title={intl.formatMessage({ id: 'admin/curbside.settings.title' })}
+          fit="none"
+        >
+          <ul className="mid-gray">
+            <li className="mb4">
+              <a href={`/admin/apps/${getAppId()}/setup`}>
+                <FormattedMessage id="admin/curbside.settings.link" />
+              </a>
+            </li>
+          </ul>
+        </Box>
+      </div>
     </Layout>
   )
 }
 
-export default CurbsideIndex
+export default injectIntl(CurbsideIndex)
